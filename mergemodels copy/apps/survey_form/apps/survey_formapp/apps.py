30bb0dd5292d7fa1d126e9cae5f4bf5eb16183af
Quint436 <quint436@uw.edu>
2017-04-16 19:39:49 -0700

    deleted files

--- mergemodels copy/apps/survey_form/apps/survey_formapp/apps.py
+++
@@ -1,7 +0,0 @@
-from __future__ import unicode_literals
-
-from django.apps import AppConfig
-
-
-class SurveyFormappConfig(AppConfig):
-    name = 'survey_formapp'
--- mysite/apps/blog/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-urlpatterns = [
-    url(r'^$', views.post_list, name='post_list'), #only and empty string will work
-
-]
